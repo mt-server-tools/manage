4da6cb1a598d96059b8982ef9a2888061230817b
Tai @ Flex <dch.tai@gmail.com>
2016-08-06 11:24:37 +0100

    installer and preliminary mtconfig

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "$UID != 0 ]]; then
+	echo "[1;31mYou are not root[0m"
+	exit 1
+fi
+
+cp bin/* /usr/local/bin
+
+cp mtmanage.conf /etc/minetest/mtmanage.conf
+chmod 644 /etc/minetest/mtmanage.conf
