17d9022ea0603caed49757351c4f0cadba98d4e7
Tai @ Flex <dch.tai@gmail.com>
2016-09-07 14:29:20 +0100

    installation script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Install mt-manage tools
+# etc/mt-manage.conf --> /etc/mt-manage.conf
+# bin/ --> $MTMPATH/bin
+# mtlib --> $MTMPATH/mtlib
+
+# install global pre-reqs
+
+apt-get install tmux htop git perl
+
