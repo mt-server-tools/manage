5565a6919a03369491349078218d2cb28d2697af
Tai @ Flex <dch.tai@gmail.com>
2017-06-10 15:43:48 +0100

    project effort reset

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# Install mt-manage tools
-# etc/mt-manage.conf --> /etc/mt-manage.conf
-# bin/ --> $MTMPATH/bin
-# mtlib --> $MTMPATH/mtlib
-
-# install global pre-reqs
-
-apt-get install tmux htop git perl
-
